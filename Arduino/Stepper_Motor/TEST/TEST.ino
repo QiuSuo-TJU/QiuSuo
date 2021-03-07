ac43c4e7e28d4511f4da7f8fa7699a55f97fb194
Orion <hezc1928@gmail.com>
2021-03-07 18:12:17 +0800

    Update TEST.ino
    update

--- Arduino/Stepper_Motor/TEST/TEST.ino
+++ Arduino/Stepper_Motor/TEST/TEST.ino
@@ -1,23 +1,23 @@
 #include <Stepper.h>
-#define in1Pin 8
-#define in2Pin 10
-#define in3Pin 9
-#define in4Pin 11
+#define VCC 2
+#define PLS 3
+#define DIR 4
+#define ENA 5//锁死，不用管它
 int StepsRequired;
 
-const int STEPS_PER_ROTOR_REV = 32;//电机内部输出轴旋转一周步数
-const int GEAR_REDUCTION = 64;
+const int STEPS_PER_ROTOR_REV = 200;//电机内部输出轴旋转一周步数
+const int GEAR_REDUCTION = 1;
 //这两行等电机到手输入实际参数
 const float STEPS_PER_OUT_REV = STEPS_PER_ROTOR_REV * GEAR_REDUCTION;
 
-Stepper steppermotor(STEPS_PER_ROTOR_REV, in1Pin, in2Pin, in3Pin, in4Pin);
+Stepper steppermotor(STEPS_PER_ROTOR_REV, PLS, 19);
 //这里需要根据驱动板实际情况调整in1Pin等的顺序
 void setup()
 {
-  pinMode(in1Pin, OUTPUT);
-  pinMode(in2Pin, OUTPUT);
-  pinMode(in3Pin, OUTPUT);
-  pinMode(in4Pin, OUTPUT);
+pinMode(VCC, OUTPUT);
+pinMode(PLS, OUTPUT);
+pinMode(DIR, OUTPUT);
+pinMode(ENA, OUTPUT);
 }
 
 void loop()
@@ -26,6 +26,8 @@ void loop()
   steppermotor.setSpeed(90);
   //这一行调速度,参数直接是转速
   steppermotor.step(StepsRequired);
+  digitalWrite(ENA,LOW);
+  digitalWrite(DIR,HIGH);
   //没加延时，无限转
   //这段内容因为是无限转所以可以直接放到setup里面
 }
